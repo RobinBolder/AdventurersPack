28534ef0b275782a94ef1e1be69b2bd5b7c4d7ce
Robin <robin.bolder@gmail.com>
2017-11-24 15:15:04 +0100

    Basic Commandline app DiceRollCmd is now functional; To be added:  - Full attack flow (d20 roll --> modifiers --> hit/miss --> x dx roll --> modifiers --> damage  - Documentation  - Unit tests

--- src/DiceRoller/App/DiceRollCmd.java
+++ src/DiceRoller/App/DiceRollCmd.java
@@ -8,11 +8,186 @@ import java.util.Scanner;
 public class DiceRollCmd {
 
     public static void main(String[] args) {
-        System.out.println("Welcome \n what kind of die do you want to roll?");
         Scanner sc = new Scanner(System.in);
-        int response = sc.nextInt();
-        DiceResult result = Dice.roll(response);
-        //System.out.println("You rolled a " + result.getTotal() + " using your d" + response);
+        int response;
+
+        System.out.println("Welcome to the DiceRoller app!\n");
+
+        do {
+            System.out.println("What would you like to do?");
+            System.out.println("1. Roll a d20");
+            System.out.println("2. Roll different dice");
+            System.out.println("3. Make an attack");
+            System.out.println("4. Close the application");
+            try {
+                response = sc.nextInt();
+                switch (response) {
+                    case 1:
+                        rollD20(sc);
+                        break;
+                    case 2:
+                        rollOther(sc);
+                        break;
+                    case 3:
+                        System.out.println("Sorry, this functionality is not yet added :(");
+                        break;
+                    case 4:
+                        System.out.println("Goodbye");
+                        break;
+                    default:
+                        System.out.println("That was not a valid answer, try again!");
+                }
+            }
+            catch (Exception ex) {
+                System.out.println("That was not a valid answer, try again!");
+                response = 0;
+            }
+
+        } while (response!=4);
+
+    }
+
+    private static void rollD20(Scanner sc) {
+        int response;
+        DiceResult result;
+        Dice d20 = new Dice(20);
+
+        System.out.println("What type of d20 roll do you want to make?");
+        System.out.println("1. Normal");
+        System.out.println("2. Advantage");
+        System.out.println("3. Disadvantage");
+        System.out.println("4. Back to main menu");
+
+        try {
+            response = sc.nextInt();
+            switch (response) {
+                case 1:
+                    result = d20.roll();
+                    System.out.println(result.getText());
+                    break;
+                case 2:
+                    result = d20.rollAdv();
+                    System.out.println(result.getText());
+                    break;
+                case 3:
+                    result = d20.rollDis();
+                    System.out.println(result.getText());
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("That was not a valid answer, try again!");
+            }
+        } catch (Exception ex) {
+            System.out.println("That was not a valid answer, try again!");
+        }
+    }
+
+    private static void rollOther(Scanner sc) {
+        int response;
+
+        System.out.println("What type of dice do you want to roll?");
+        System.out.println("1. Standard");
+        System.out.println("2. Custom");
+        System.out.println("3. Back to main menu");
+        do {
+            try {
+                response = sc.nextInt();
+                switch (response) {
+                    case 1:
+                        rollStandard(sc);
+                        response = 3;
+                        break;
+                    case 2:
+                        rollCustom(sc);
+                        response = 3;
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        System.out.println("That was not a valid answer, try again!");
+                }
+            } catch (Exception ex) {
+                System.out.println("That was not a valid answer, try again!");
+                response = 0;
+            }
+        } while (response !=3);
+
+    }
+
+    private static void rollStandard(Scanner sc) {
+        Dice dice = getDiceType(sc);
+        int number = getNumber(sc);
+        DiceResult result = dice.rollMultiple(number);
         System.out.println(result.getText());
     }
+
+    private static void rollCustom(Scanner sc) {
+        Dice dice = getnSides(sc);
+        int number = getNumber(sc);
+        DiceResult result = dice.rollMultiple(number);
+        System.out.println(result.getText());
+    }
+
+    private static Dice getDiceType(Scanner sc) {
+        int response;
+
+        System.out.println("What type of dice do you want to roll?");
+        System.out.println("1. d4");
+        System.out.println("2. d6");
+        System.out.println("3. d8");
+        System.out.println("4. d10");
+        System.out.println("5. d12");
+        System.out.println("6. d100");
+        System.out.println("7. Other");
+
+        do {
+            try {
+                response = sc.nextInt();
+                switch (response) {
+                    case 1:
+                        return new Dice(4);
+                    case 2:
+                        return new Dice(6);
+                    case 3:
+                        return new Dice(8);
+                    case 4:
+                        return new Dice(10);
+                    case 5:
+                        return new Dice(12);
+                    case 6:
+                        return new Dice(100);
+                    case 7:
+                        return getnSides(sc);
+                    default:
+                        System.out.println("That was not a valid answer, try again!");
+                }
+            } catch (Exception ex) {
+                System.out.println("That was not a valid answer, try again!");
+            }
+        } while(true);
+    }
+
+    private static int getNumber(Scanner sc) {
+        System.out.println("How many of these dice do you want to roll?");
+        do {
+            try {
+                return sc.nextInt();
+            } catch (Exception ex) {
+                System.out.println("That was not a valid answer, try again!");
+            }
+        } while (true);
+    }
+
+    private static Dice getnSides(Scanner sc) {
+        System.out.println("How many sides do you want your dice to have?");
+        do {
+            try {
+                return new Dice(sc.nextInt());
+            } catch (Exception ex) {
+                System.out.println("That was not a valid answer, try again!");
+            }
+        } while (true);
+    }
+
 }
--- src/DiceRoller/Dice.java
+++ src/DiceRoller/Dice.java
@@ -22,4 +22,53 @@ public class Dice {
         String resultString = ("You rolled a d" + sides + " and got " + resultInt);
         return new DiceResult(resultInt, resultString);
     }
+
+    public DiceResult rollAdv() {
+        int total;
+        DiceResult result1 = roll();
+        DiceResult result2 = roll();
+
+        if (result1.getTotal() > result2.getTotal()) {
+            total = result1.getTotal();
+        }
+        else {
+            total = result2.getTotal();
+        }
+        String resultString = ("You rolled a d" + sides + " with advantage and got " +
+                result1.getTotal() + " and " + result2.getTotal() + ";\n" +
+                "The result therefore is: " + total);
+        return new DiceResult(total, resultString);
+    }
+
+    public DiceResult rollDis() {
+        int total;
+        DiceResult result1 = roll();
+        DiceResult result2 = roll();
+
+        if (result1.getTotal() < result2.getTotal()) {
+            total = result1.getTotal();
+        }
+        else {
+            total = result2.getTotal();
+        }
+        String resultString = ("You rolled a d" + sides + " with disadvantage and got " +
+                result1.getTotal() + " and " + result2.getTotal() + ";\n" +
+                "The result therefore is: " + total);
+        return new DiceResult(total, resultString);
+    }
+
+    public DiceResult rollMultiple(int n) {
+        StringBuilder sb = new StringBuilder();
+        DiceResult tmp;
+        int total = 0;
+        sb.append("You rolled " + n + " d" + sides + "'s\n");
+        for (int i = 0; i < n; i++) {
+            tmp = roll();
+            total += tmp.getTotal();
+            sb.append("You rolled a " + tmp.getTotal() + " on dice number " + (i+1) + "\n");
+        }
+        sb.append("Your total result is " + total);
+        String text = sb.toString();
+        return new DiceResult(total,text);
+    }
 }
