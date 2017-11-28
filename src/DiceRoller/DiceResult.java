2fec4c338fe74c49e6be9c09412861343b82eb9d
Robin <robin.bolder@gmail.com>
2017-11-28 19:03:14 +0100

    Added functionality to the DiceResult class: - add method for easily adding DiceResults together - allValues ArrayList containing all the individual dice result values (integers) that were added together in the current DiceResult object    - getAllValues method for retrieving the list - setText method for overwriting the text string

--- src/DiceRoller/DiceResult.java
+++ src/DiceRoller/DiceResult.java
@@ -2,17 +2,22 @@ package DiceRoller;
 
 //import java.util.List;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Simple container class to store the results of a dice roll or a series of rolls, both in numerical value and as
  * a text string telling the user what happened.
  *
  * @author Robin Bolder
- * @version 1.0
+ * @version 1.1
  */
 public class DiceResult {
     private String text;
     private int total;
-    //private List allResults;  // Can be used to store a complete list of all the different roll values
+    private List<Integer> allValues;
+    //private List allResults;  // Can be used to store a complete list of all the different DiceResults combined in the total
+    //TODO add a list with all the individual DiceResults that were added/combined into this final DiceResult
 
     /**
      * Main container constructor
@@ -23,6 +28,8 @@ public class DiceResult {
     public DiceResult(int result, String text) {
         this.total = result;
         this.text = text;
+        this.allValues = new ArrayList<>();
+        allValues.add(result);
     }
 
     /**
@@ -31,6 +38,7 @@ public class DiceResult {
     public DiceResult() {
         this.total = 0;
         this.text = "";
+        this.allValues = new ArrayList<>();
     }
 
     /**
@@ -50,11 +58,40 @@ public class DiceResult {
     }
 
     /**
+     * @return An ArrayList containing all the values
+     * @since 1.1
+     */
+    public List getAllValues() { return allValues; }
+
+    /**
      * Add the values of another dice roll to this object
      * @param additionalResult DiceResult object of which the values will be added to the existing one
+     * @since 1.1
      */
     public void add(DiceResult additionalResult) {
+        for (Object other : additionalResult.getAllValues()) {
+            /*if (!(other instanceof Integer)){
+                throw new IllegalArgumentException();
+            }*/
+            Integer otherValue = (Integer) other;
+            this.allValues.add(otherValue);
+        }
+
         this.total += additionalResult.getTotal();
-        this.text += "\n" + additionalResult.getText();
+        if (this.text.equals("")) {
+            this.text = additionalResult.getText();
+        }
+        else{
+            this.text += "\n" + additionalResult.getText();
+        }
+    }
+
+    /**
+     * Overwrite the original text string with new text
+     * @param text String containing the new text
+     * @since 1.1
+     */
+    public void setText(String text) {
+        this.text = text;
     }
 }
